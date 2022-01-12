6ab93b05be0da3fb23d443863a21d30b497d96b6
HaneenJer <haneenjeries65@gmail.com>
2022-01-12 22:57:01 +0200

    Update MinimaxPlayer.py

--- players/MinimaxPlayer.py
+++ players/MinimaxPlayer.py
@@ -1,14 +1,20 @@
 """
 MiniMax Player
 """
+import numpy as np
+
 from players.AbstractPlayer import AbstractPlayer
-#TODO: you can import more modules, if needed
+
+
+# TODO: you can import more modules, if needed
 
 class Player(AbstractPlayer):
     def __init__(self, game_time):
-        AbstractPlayer.__init__(self, game_time) # keep the inheritance of the parent's (AbstractPlayer) __init__()
-        #TODO: initialize more fields, if needed, and the AlphaBeta algorithm from SearchAlgos.py
-
+        AbstractPlayer.__init__(self, game_time)  # keep the inheritance of the parent's (AbstractPlayer) __init__()
+        # TODO: initialize more fields, if needed, and the AlphaBeta algorithm from SearchAlgos.py
+        self.turn = 0
+        self.my_pos = None
+        self.rival_pos = None
 
     def set_game_params(self, board):
         """Set the game parameters needed for this player.
@@ -19,6 +25,9 @@ class Player(AbstractPlayer):
         No output is expected.
         """
         # TODO: erase the following line and implement this function.
+        self.my_pos = np.full(9, -1)
+        self.rival_pos = np.full(9, -1)
+        self.board = board
         raise NotImplementedError
 
     def make_move(self, time_limit):
@@ -28,7 +37,11 @@ class Player(AbstractPlayer):
         output:
             - direction: tuple, specifing the Player's movement
         """
-        #TODO: erase the following line and implement this function.
+        if self.turn < 18:
+            move = self._stage_1_move()
+            self.turn += 1
+            return move
+        # TODO: erase the following line and implement this function.
         raise NotImplementedError
 
     def set_rival_move(self, move):
@@ -40,12 +53,35 @@ class Player(AbstractPlayer):
         # TODO: erase the following line and implement this function.
         raise NotImplementedError
 
-
-
     ########## helper functions in class ##########
     # TODO: add here helper functions in class, if needed
 
+    def _stage_1_choose_cell_and_soldier_to_move(self):
+        # TODO: choose cell to move
+        # TODO: choose soldier
+        cell = None
+        soldier_that_moved = None
+        return cell, soldier_that_moved
 
+    def _choose_rival_cell_to_kill(self):
+        # TODO: choose cell heuristic
+        rival_cell = None
+        return rival_cell
+
+    def _make_mill_get_rival_cell(self):
+        rival_cell = self._choose_rival_cell_to_kill()
+        rival_idx = np.where(self.rival_pos == rival_cell)[0][0]
+        self.rival_pos[rival_idx] = -2
+        self.board[rival_cell] = 0
+        return rival_cell
+
+    def _stage_1_move(self) -> tuple:
+        cell, soldier_that_moved = self._stage_1_choose_cell_and_soldier_to_move()
+        self.my_pos[soldier_that_moved] = cell
+        self.board[cell] = 1
+
+        rival_cell = -1 if not self.is_mill(cell) else self._make_mill_get_rival_cell()
+        return cell, soldier_that_moved, rival_cell
 
     ########## helper functions for AlphaBeta algorithm ##########
     # TODO: add here the utility, succ, an
